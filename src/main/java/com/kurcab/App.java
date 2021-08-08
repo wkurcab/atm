340c04b0f3228eecfd5a096c96d4ff4bf034974b
wkurcab <wojciech@kurcab.com>
2021-08-08 16:43:51 +0200

    First Commit

--- src/main/java/com/kurcab/App.java
+++ src/main/java/com/kurcab/App.java
@@ -1,16 +1,11 @@
 package com.kurcab;
 
-import com.google.gson.Gson;
-import lombok.AllArgsConstructor;
-
 /**
  * Hello world!
  *
  */
-@AllArgsConstructor
 public class App {
     public static void main( String[] args ) {
-        Gson gson = new Gson();
         System.out.println( "Hello World!" );
     }
 }
